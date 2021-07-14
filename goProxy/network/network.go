d3443e0fc2634ed96e62f2bb54b91188472be05f
wjordansun <56767177+wjordansun@users.noreply.github.com>
2021-07-14 17:34:43 -0400

    Add files via upload

---
+++ goProxy/network/network.go
@@ -0,0 +1,90 @@
+package network
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+)
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func ChangePort(conn net.Conn) {
+	req, err := ioutil.ReadAll(conn)
+	Handle(err)
+	defer conn.Close()
+	fmt.Println(req)
+
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+	logger := log.WithFields(log.Fields{
+		"service": "go-reverse-proxy",
+	})
+
+	Router := httprouter.New()
+	origin, _ := url.Parse("http://localhost:8080/")
+	path := "/*catchall"
+
+	reverseProxy := httputil.NewSingleHostReverseProxy(origin)
+
+	reverseProxy.Director = func(req *http.Request) {
+		req.Header.Add("X-Forwarded-Host", req.Host)
+		req.Header.Add("X-Origin-Host", origin.Host)
+		req.URL.Scheme = origin.Scheme
+		req.URL.Host = origin.Host
+
+		wildcardIndex := strings.IndexAny(path, "*")
+		proxyPath := singleJoiningSlash(origin.Path, req.URL.Path[wildcardIndex:])
+		if strings.HasSuffix(proxyPath, "/") && len(proxyPath) > 1 {
+			proxyPath = proxyPath[:len(proxyPath)-1]
+		}
+		req.URL.Path = proxyPath
+	}
+
+	Router.Handle("GET", path, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		reverseProxy.ServeHTTP(w, r)
+	})
+
+	logger.Fatal(http.ListenAndServe(":9002", Router))
+
+}
+
+
+
+func StartServer() {
+	ln, err := net.Listen("tcp", "localhost:8000")
+	Handle(err)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		go ChangePort(conn)
+
+	}
+}
